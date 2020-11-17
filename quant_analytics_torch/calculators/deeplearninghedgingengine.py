6f06151c6308370cb522731907ed8013493ccb7e
Quant Analytist <quant.analytics.torch@gmail.com>
2020-11-17 23:35:34 +0000

    Only draw the simulations once

--- quant_analytics_torch/calculators/deeplearninghedgingengine.py
+++ quant_analytics_torch/calculators/deeplearninghedgingengine.py
@@ -4,7 +4,7 @@ from quant_analytics_torch.modules.longshorttermmemory import LongShortTermModul
 from quant_analytics_torch.calculators.pathgeneratornormal import PathGeneratorNormal
 
 class DeepLearningValuationEngine(torch.nn.Module):
-    def __init__(self, input_size=1, batch_size=1000, hidden_layer_size=128, output_size=1, epochs=20, training_size=50, seq_len = 2):
+    def __init__(self, input_size=1, batch_size=1000, hidden_layer_size=128, output_size=1, epochs=20, training_size=15, seq_len = 2):
         super().__init__()
 
         self.batch_size = batch_size
@@ -14,7 +14,7 @@ class DeepLearningValuationEngine(torch.nn.Module):
         self.seq_len = seq_len
 
         self.model = LongShortTermModule(batch_size=self.batch_size, hidden_layer_size=self.hidden_layer_size)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0005)
 
         self.input_dim = 1
         self.risk_aversion = 20
@@ -48,16 +48,25 @@ class DeepLearningValuationEngine(torch.nn.Module):
 
     def run(self):
 
+        # Total number of Monte-Carlo simulations
+        simulation_size = self.batch_size * self.training_size
+
+        # Only do one simulation at the beginning
+        x0_t = self.paths(simulation_size)
+
         for i in range(self.epochs):
+
+            self.model.train()
+
             for j in range(self.training_size):
 
                 self.optimizer.zero_grad()
-                #self.model.hidden_cell = (  torch.zeros(1, self.model.batch_size, self.model.hidden_layer_size),
-                #                            torch.zeros(1, self.model.batch_size,  self.model.hidden_layer_size))
 
+                # Reinitialise the hidden cells
                 self.init_hidden(self.batch_size)
 
-                x0 = self.paths(self.batch_size)
+                # Take a subset of the paths
+                x0 = x0_t[j*self.batch_size:(j+1)*self.batch_size,:,:]
 
                 # Payoff being pi = max(dx-0,0)
                 payoff = self.payoff(x0)
--- quant_analytics_torch/sandbox/one_step_bachelier.py
+++ quant_analytics_torch/sandbox/one_step_bachelier.py
@@ -1,6 +1,7 @@
 import torch
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 from quant_analytics_torch.modules.longshorttermmemory import LongShortTermModule
 
@@ -10,38 +11,43 @@ def entropy_loss(x,d=20):
 if __name__ == '__main__':
     print('Quantitative analyst deep learning')
 
-    batch_size = 1000
+    training_size = 20
+    batch_size = 10**3
+    simulations = training_size * batch_size
 
     model = LongShortTermModule(batch_size=batch_size)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     epochs = 20
-    training_size = 50
     seq_len = 2
     input_dim = 1
     risk_aversion = 10
     sigma = 0.2
 
+
+    # Initial model price is 0
+    x0 = torch.zeros(simulations, seq_len + 1, input_dim)
+
+    # One step simulation
+    dx = torch.randn(simulations, seq_len, input_dim)
+
+    for k in range(seq_len):
+        x0[:,k+1,:] = x0[:,k,:] + sigma * dx[:,k,:]
+
+
     for i in range(epochs):
+        model.train()
+
         for j in range(training_size):
 
             optimizer.zero_grad()
             model.hidden_cell = (torch.zeros(1, model.batch_size, model.hidden_layer_size),
                         torch.zeros(1, model.batch_size, model.hidden_layer_size))
 
-            # Initial model price is 0
-            x0 = torch.zeros(batch_size, seq_len + 1, input_dim)
-
-            # One step simulation
-            dx = torch.randn(batch_size, seq_len, input_dim)
-
-            for k in range(seq_len):
-                x0[:,k+1,:] = x0[:,k,:] + sigma * dx[:,k,:]
-
-            delta = model(x0[:,:-1,:])
+            delta = model(x0[j*batch_size:(j+1)*batch_size,:-1,:])
 
             # Payoff being pi = max(dx-0,0)
-            payoff = torch.max(x0[:,-1,:],torch.tensor(0.))
+            payoff = torch.max(x0[j*batch_size:(j+1)*batch_size,-1,:],torch.tensor(0.))
 
             # Hedge performance pnl = pi - delta * dx
             
@@ -49,10 +55,10 @@ if __name__ == '__main__':
             delta_zero = torch.zeros(batch_size, input_dim)
 
             for k in range(seq_len):
-                pnl = pnl + (delta[:,k,:]-delta_zero)  * x0[:,k,:]
+                pnl = pnl + (delta[:,k,:]-delta_zero)  * x0[j*batch_size:(j+1)*batch_size,k,:]
                 delta_zero = delta[:,k,:]
 
-            pnl = pnl + payoff - delta[:,-1,:]*x0[:,-1,:]
+            pnl = pnl + payoff - delta[:,-1,:]*x0[j*batch_size:(j+1)*batch_size,-1,:]
 
             # Loss function is a mean variance optimisation
             loss = -torch.mean(pnl) + risk_aversion * torch.norm(pnl)
