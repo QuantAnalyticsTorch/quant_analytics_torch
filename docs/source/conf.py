c08940ac4996324be8093ce70ba17eaf63428e79
Quant Analytist <quant.analytics.torch@gmail.com>
2021-12-03 09:40:07 +0000

    Update the search engine

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Quant Analytics'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon','hoverxref.extension',"sphinx_design", "nbsphinx", 'sphinx_search.extension']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon','hoverxref.extension',"sphinx_design", "nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,6 +51,8 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+templates_path = ["_templates"]
 
 autodoc_default_options = {
     'members': True,
