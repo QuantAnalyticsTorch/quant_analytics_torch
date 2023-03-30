8debb595b1ed281879306f2f0a579a38cbfb905a
Quant Analytics Torch Deep Learning <73231285+QuantAnalyticsTorch@users.noreply.github.com>
2023-03-30 12:06:23 +0000

    Update on the Heston Simulation

--- quant_analytics_torch/analytics/norminv.py
+++ quant_analytics_torch/analytics/norminv.py
@@ -4,4 +4,17 @@ import numpy as np
 SQRT_2 = np.sqrt(2.)
 
 def norminv(x):
+    """ Inverse cumulative normal function`
+    
+    .. _inverse_cumulative_normal:
+    
+    """
     return torch.erfinv(2.*(x-0.5))*SQRT_2
+
+def cumnorm(x):
+    """ Cumulative normal function`
+    
+    .. _cumulative_normal:
+    
+    """
+    return (1.0 + torch.erf(x / SQRT_2)) / 2.0
